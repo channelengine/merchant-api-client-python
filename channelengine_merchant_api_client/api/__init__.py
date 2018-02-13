6bc996bcdc6eeae3e83e4406353e8eec623c846c
Christiaan de Ridder <ik@christiaanderidder.nl>
2018-02-13 14:24:24 +0100

    Generate version 2.5.0

---
+++ channelengine_merchant_api_client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from channelengine_merchant_api_client.api.cancellation_api import CancellationApi
+from channelengine_merchant_api_client.api.offer_api import OfferApi
+from channelengine_merchant_api_client.api.order_api import OrderApi
+from channelengine_merchant_api_client.api.product_api import ProductApi
+from channelengine_merchant_api_client.api.return_api import ReturnApi
+from channelengine_merchant_api_client.api.shipment_api import ShipmentApi
