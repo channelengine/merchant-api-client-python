b42bcfaab79eb18fd00e07e1f4ddbd1d6c313c7e
Christiaan de Ridder <christiaan@channelengine.com>
2021-06-18 13:30:15 +0200

    Generate version 2.9.7

--- channelengine_merchant_api_client/api/__init__.py
+++ channelengine_merchant_api_client/api/__init__.py
@@ -1,16 +1,3 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from channelengine_merchant_api_client.api.cancellation_api import CancellationApi
-from channelengine_merchant_api_client.api.channels_api import ChannelsApi
-from channelengine_merchant_api_client.api.listed_products_api import ListedProductsApi
-from channelengine_merchant_api_client.api.notification_api import NotificationApi
-from channelengine_merchant_api_client.api.offer_api import OfferApi
-from channelengine_merchant_api_client.api.order_api import OrderApi
-from channelengine_merchant_api_client.api.product_api import ProductApi
-from channelengine_merchant_api_client.api.product_bundle_api import ProductBundleApi
-from channelengine_merchant_api_client.api.return_api import ReturnApi
-from channelengine_merchant_api_client.api.shipment_api import ShipmentApi
-from channelengine_merchant_api_client.api.stock_location_api import StockLocationApi
+# do not import all apis into this module because that uses a lot of memory and stack frames
+# if you need the ability to import all apis from one package, import them with
+# from channelengine_merchant_api_client.apis import CancellationApi
---
+++ channelengine_merchant_api_client/apis/__init__.py
@@ -0,0 +1,27 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.cancellation_api import CancellationApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from channelengine_merchant_api_client.api.cancellation_api import CancellationApi
+from channelengine_merchant_api_client.api.channels_api import ChannelsApi
+from channelengine_merchant_api_client.api.listed_products_api import ListedProductsApi
+from channelengine_merchant_api_client.api.notification_api import NotificationApi
+from channelengine_merchant_api_client.api.offer_api import OfferApi
+from channelengine_merchant_api_client.api.order_api import OrderApi
+from channelengine_merchant_api_client.api.product_api import ProductApi
+from channelengine_merchant_api_client.api.product_bundle_api import ProductBundleApi
+from channelengine_merchant_api_client.api.return_api import ReturnApi
+from channelengine_merchant_api_client.api.shipment_api import ShipmentApi
+from channelengine_merchant_api_client.api.stock_location_api import StockLocationApi
