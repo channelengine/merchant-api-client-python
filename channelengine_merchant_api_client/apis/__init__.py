a11c83052b66480b063eb3dbd6048bb3a955c934
Tomek M <tomasz@channelengine.com>
2021-10-14 15:42:23 +0200

    Generate version 2.9.9

--- channelengine_merchant_api_client/apis/__init__.py
+++ channelengine_merchant_api_client/apis/__init__.py
@@ -16,6 +16,7 @@
 # Import APIs into API package:
 from channelengine_merchant_api_client.api.cancellation_api import CancellationApi
 from channelengine_merchant_api_client.api.channels_api import ChannelsApi
+from channelengine_merchant_api_client.api.competition_price_api import CompetitionPriceApi
 from channelengine_merchant_api_client.api.listed_products_api import ListedProductsApi
 from channelengine_merchant_api_client.api.notification_api import NotificationApi
 from channelengine_merchant_api_client.api.offer_api import OfferApi
@@ -23,6 +24,7 @@ from channelengine_merchant_api_client.api.order_api import OrderApi
 from channelengine_merchant_api_client.api.product_api import ProductApi
 from channelengine_merchant_api_client.api.product_bundle_api import ProductBundleApi
 from channelengine_merchant_api_client.api.return_api import ReturnApi
+from channelengine_merchant_api_client.api.settings_api import SettingsApi
 from channelengine_merchant_api_client.api.shipment_api import ShipmentApi
 from channelengine_merchant_api_client.api.stock_location_api import StockLocationApi
 from channelengine_merchant_api_client.api.webhook_api import WebhookApi
